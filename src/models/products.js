e2215d3a2367db3466ac72fe1e1ef940b9887cba
OrionFerrin <138627714+OrionFerrin@users.noreply.github.com>
2025-07-30 17:00:32 -0500

    añadir producto

--- src/models/products.js
+++ src/models/products.js
@@ -4,7 +4,8 @@ const products = [
   { id: 3, name: "Chocolate Muffin", price: 1.8 },
   { id: 4, name: "Chicken Wrap", price: 3.0 },
   { id: 5, name: "Fruit Salad", price: 2.0 },
-  { id: 6, name: "Coffee", price: 1.5 }
+  { id: 6, name: "Coffee", price: 1.5 },
+  { id: 7, name: "Energy drink", price: 1 }
 ];
 
 module.exports = products;
